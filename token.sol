45ea7cf2e225d88f7f1e73da6b1076aa67842982
"SandovaI" <"calebsandoval901@gmail.com">
2022-02-12 15:51:40 -0800

    minted

--- token.sol
+++ token.sol
@@ -2,13 +2,20 @@ pragma solidity ^0.8.10;
 
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
 
-contract stayHardToken is ERC20{
+contract StayHardToken is ERC20{
 
+    mapping(address => uint256) private _balances;
+
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
 
     constructor() ERC20('STAY HARD Token', 'HARD'){
 
-        _mint(msg.sender, 1000 * 10 ** 18);
-        
+        _mint(msg.sender, 100 * 10 ** 18 );                                      
     }
    
 
